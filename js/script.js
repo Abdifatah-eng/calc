11da8f74fce2b6140cbecc36a17561d246bd49fd
Abdifatah-eng <hiiliyelohos@gmail.com>
2020-02-13 10:55:14 +0300

    my first calculator initial page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var add = function(number1,  number2) {
+return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter Number:"));
+var number2 = parseInt(prompt("Enter Number:"));
+alert(add(number1, number2));
