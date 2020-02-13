15c22811bf1c2c4fa8211d880dd2413f79ff0c36
Abdifatah-eng <hiiliyelohos@gmail.com>
2020-02-13 12:10:53 +0300

    calculator with all operators

--- js/script.js
+++ js/script.js
@@ -2,5 +2,24 @@ var add = function(number1,  number2) {
 return number1 + number2;
 };
 var number1 = parseInt(prompt("Enter Number:"));
-var number2 = parseInt(prompt("Enter Number:"));
-alert(add(number1, number2));
+ var number2 = parseInt(prompt("Enter Number:"));
+   alert(add(number1, number2));
+
+      var subtract = function(number1, number2) {
+        return number1 - number2;
+         };
+          var number1 = prompt("Enter Number for subtract:");
+           var number2 = prompt("Enter Number for subtract:");
+          alert(subtract(number1, number2));
+       var multiply = function (number1, number2) {
+    return number1 * number2;
+  }
+  var number1 = prompt("for multiplication:");
+var number2 = prompt("for multiplication:");
+alert(multiply(number1, number2));
+var divide = function(number1, number2){
+return number1 / number2;
+ }
+ var number1 = prompt("dividing numbers:");
+ var number2 = prompt("dividing numbers:");
+ alert(divide(number1, number2));
